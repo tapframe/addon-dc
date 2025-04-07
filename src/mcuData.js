7ad8df32a21392650b6096c510daef8cdb306072
GitHub Action <action@github.com>
2025-04-07 23:53:17 +0000

    Monthly MCU data update

--- src/mcuData.js
+++ src/mcuData.js
@@ -1,55 +1,320 @@
 module.exports = [
-  { title: "Captain America: The First Avenger", type: "movie", tmdbId: 1771, releaseDate: "2011-07-22" },
-  { title: "Captain Marvel", type: "movie", tmdbId: 299537, releaseDate: "2019-03-08" },
-  { title: "Iron Man", type: "movie", tmdbId: 1726, releaseDate: "2008-05-02" },
-  { title: "The Incredible Hulk", type: "movie", tmdbId: 1724, releaseDate: "2008-06-13" },
-  { title: "Iron Man 2", type: "movie", tmdbId: 10138, releaseDate: "2010-05-07" },
-  { title: "Thor", type: "movie", tmdbId: 10195, releaseDate: "2011-05-06" },
-  { title: "The Avengers", type: "movie", tmdbId: 24428, releaseDate: "2012-05-04" },
-  { title: "Iron Man 3", type: "movie", tmdbId: 68721, releaseDate: "2013-05-03" },
-  { title: "Thor: The Dark World", type: "movie", tmdbId: 76338, releaseDate: "2013-11-08" },
-  { title: "Captain America: The Winter Soldier", type: "movie", tmdbId: 100402, releaseDate: "2014-04-04" },
-  { title: "Guardians of the Galaxy", type: "movie", tmdbId: 118340, releaseDate: "2014-08-01" },
-  { title: "Guardians of the Galaxy Vol. 2", type: "movie", tmdbId: 283995, releaseDate: "2017-05-05" },
-  { title: "Avengers: Age of Ultron", type: "movie", tmdbId: 99861, releaseDate: "2015-05-01" },
-  { title: "Ant-Man", type: "movie", tmdbId: 102899, releaseDate: "2015-07-17" },
-  { title: "Captain America: Civil War", type: "movie", tmdbId: 271110, releaseDate: "2016-05-06" },
-  { title: "Black Widow", type: "movie", tmdbId: 497698, releaseDate: "2021-07-09" },
-  { title: "Black Panther", type: "movie", tmdbId: 284054, releaseDate: "2018-02-16" },
-  { title: "Spider-Man: Homecoming", type: "movie", tmdbId: 315635, releaseDate: "2017-07-07" },
-  { title: "Doctor Strange", type: "movie", tmdbId: 284052, releaseDate: "2016-11-04" },
-  { title: "Thor: Ragnarok", type: "movie", tmdbId: 284053, releaseDate: "2017-11-03" },
-  { title: "Avengers: Infinity War", type: "movie", tmdbId: 299536, releaseDate: "2018-04-27" },
-  { title: "Ant-Man and the Wasp", type: "movie", tmdbId: 363088, releaseDate: "2018-07-06" },
-  { title: "Avengers: Endgame", type: "movie", tmdbId: 299534, releaseDate: "2019-04-26" },
-  { title: "Spider-Man: Far From Home", type: "movie", tmdbId: 429617, releaseDate: "2019-07-02" },
-  { title: "WandaVision", type: "series", tmdbId: 85271, releaseDate: "2021-01-15" },
-  { title: "The Falcon and the Winter Soldier", type: "series", tmdbId: 88396, releaseDate: "2021-03-19" },
-  { title: "Loki", type: "series", tmdbId: 84958, releaseDate: "2021-06-09" },
-  { title: "What If...?", type: "series", tmdbId: 91363, releaseDate: "2021-08-11" },
-  { title: "Shang-Chi and the Legend of the Ten Rings", type: "movie", tmdbId: 566525, releaseDate: "2021-09-03" },
-  { title: "Eternals", type: "movie", tmdbId: 524434, releaseDate: "2021-11-05" },
-  { title: "Hawkeye", type: "series", tmdbId: 91239, releaseDate: "2021-11-24" },
-  { title: "Spider-Man: No Way Home", type: "movie", tmdbId: 634649, releaseDate: "2021-12-17" },
-  { title: "Moon Knight", type: "series", tmdbId: 92749, releaseDate: "2022-03-30" },
-  { title: "Doctor Strange in the Multiverse of Madness", type: "movie", tmdbId: 453395, releaseDate: "2022-05-06" },
-  { title: "Ms. Marvel", type: "series", tmdbId: 92782, releaseDate: "2022-06-08" },
-  { title: "Thor: Love and Thunder", type: "movie", tmdbId: 616037, releaseDate: "2022-07-08" },
-  { title: "She-Hulk: Attorney at Law", type: "series", tmdbId: 92783, releaseDate: "2022-08-18" },
-  { title: "Werewolf by Night", type: "movie", tmdbId: 719256, releaseDate: "2022-10-07" },
-  { title: "Black Panther: Wakanda Forever", type: "movie", tmdbId: 505642, releaseDate: "2022-11-11" },
-  { title: "The Guardians of the Galaxy Holiday Special", type: "movie", tmdbId: 774752, releaseDate: "2022-11-25" },
-  { title: "Ant-Man and the Wasp: Quantumania", type: "movie", tmdbId: 640146, releaseDate: "2023-02-17" },
-  { title: "Guardians of the Galaxy Vol. 3", type: "movie", tmdbId: 447365, releaseDate: "2023-05-05" },
-  { title: "Secret Invasion", type: "series", tmdbId: 114472, releaseDate: "2023-06-21" },
-  { title: "The Marvels", type: "movie", tmdbId: 609681, releaseDate: "2023-11-10" },
-  { title: "Echo", type: "series", tmdbId: 114107, releaseDate: "2024-01-09" },
-  { title: "Deadpool & Wolverine", type: "movie", tmdbId: 533535, releaseDate: "2024-07-26" },
-  { title: "Agatha All Along", type: "series", tmdbId: 138501, releaseDate: "2024-09-18" },
-  { title: "Captain America: Brave New World", type: "movie", tmdbId: 663712, releaseDate: "2025-02-14" },
-  { title: "Thunderbolts*", type: "movie", tmdbId: 1003598, releaseDate: "2025-05-02" },
-  { title: "The Fantastic Four: First Steps", type: "movie", tmdbId: 873972, releaseDate: "2025-07-25" },
-  { title: "Blade", type: "movie", tmdbId: 617332, releaseDate: "2025-11-07" },
-  { title: "Ironheart", type: "series", tmdbId: 115199, releaseDate: "2025-09-03" },
-  { title: "Daredevil: Born Again", type: "series", tmdbId: 115793, releaseDate: "2025-03-04" }
+  {
+    "title": "The Incredible Hulk",
+    "type": "movie",
+    "tmdbId": 65294,
+    "releaseDate": "1977-11-04"
+  },
+  {
+    "title": "The Avengers",
+    "type": "movie",
+    "tmdbId": 9320,
+    "releaseDate": "1998-08-13"
+  },
+  {
+    "title": "Blade",
+    "type": "movie",
+    "tmdbId": 36647,
+    "releaseDate": "1998-08-21"
+  },
+  {
+    "title": "Iron Man",
+    "type": "movie",
+    "tmdbId": 1726,
+    "releaseDate": "2008-04-30"
+  },
+  {
+    "title": "Iron Man 2",
+    "type": "movie",
+    "tmdbId": 10138,
+    "releaseDate": "2010-04-28"
+  },
+  {
+    "title": "Thor",
+    "type": "movie",
+    "tmdbId": 10195,
+    "releaseDate": "2011-04-21"
+  },
+  {
+    "title": "Captain America: The First Avenger",
+    "type": "movie",
+    "tmdbId": 1771,
+    "releaseDate": "2011-07-22"
+  },
+  {
+    "title": "Iron Man 3",
+    "type": "movie",
+    "tmdbId": 68721,
+    "releaseDate": "2013-04-18"
+  },
+  {
+    "title": "Thor: The Dark World",
+    "type": "movie",
+    "tmdbId": 76338,
+    "releaseDate": "2013-10-30"
+  },
+  {
+    "title": "Guardians of the Galaxy",
+    "type": "movie",
+    "tmdbId": 118340,
+    "releaseDate": "2014-07-30"
+  },
+  {
+    "title": "Avengers: Age of Ultron",
+    "type": "movie",
+    "tmdbId": 99861,
+    "releaseDate": "2015-04-22"
+  },
+  {
+    "title": "Ant-Man",
+    "type": "movie",
+    "tmdbId": 102899,
+    "releaseDate": "2015-07-14"
+  },
+  {
+    "title": "Captain America: Civil War",
+    "type": "movie",
+    "tmdbId": 271110,
+    "releaseDate": "2016-04-27"
+  },
+  {
+    "title": "Doctor Strange",
+    "type": "movie",
+    "tmdbId": 284052,
+    "releaseDate": "2016-10-25"
+  },
+  {
+    "title": "Guardians of the Galaxy Vol. 2",
+    "type": "movie",
+    "tmdbId": 283995,
+    "releaseDate": "2017-04-19"
+  },
+  {
+    "title": "Spider-Man: Homecoming",
+    "type": "movie",
+    "tmdbId": 315635,
+    "releaseDate": "2017-07-05"
+  },
+  {
+    "title": "Thor: Ragnarok",
+    "type": "movie",
+    "tmdbId": 284053,
+    "releaseDate": "2017-10-02"
+  },
+  {
+    "title": "Black Panther",
+    "type": "movie",
+    "tmdbId": 284054,
+    "releaseDate": "2018-02-13"
+  },
+  {
+    "title": "Avengers: Infinity War",
+    "type": "movie",
+    "tmdbId": 299536,
+    "releaseDate": "2018-04-25"
+  },
+  {
+    "title": "Ant-Man and the Wasp",
+    "type": "movie",
+    "tmdbId": 363088,
+    "releaseDate": "2018-07-04"
+  },
+  {
+    "title": "Captain Marvel",
+    "type": "movie",
+    "tmdbId": 299537,
+    "releaseDate": "2019-03-06"
+  },
+  {
+    "title": "Avengers: Endgame",
+    "type": "movie",
+    "tmdbId": 299534,
+    "releaseDate": "2019-04-24"
+  },
+  {
+    "title": "Spider-Man: Far From Home",
+    "type": "movie",
+    "tmdbId": 429617,
+    "releaseDate": "2019-06-28"
+  },
+  {
+    "title": "WandaVision",
+    "type": "series",
+    "tmdbId": 85271,
+    "releaseDate": "2021-01-15"
+  },
+  {
+    "title": "Captain America: The Winter Soldier",
+    "type": "series",
+    "tmdbId": 88396,
+    "releaseDate": "2021-03-19"
+  },
+  {
+    "title": "The Falcon and the Winter Soldier",
+    "type": "series",
+    "tmdbId": 88396,
+    "releaseDate": "2021-03-19"
+  },
+  {
+    "title": "Loki",
+    "type": "series",
+    "tmdbId": 84958,
+    "releaseDate": "2021-06-09"
+  },
+  {
+    "title": "Black Widow",
+    "type": "movie",
+    "tmdbId": 497698,
+    "releaseDate": "2021-07-07"
+  },
+  {
+    "title": "What If...?",
+    "type": "series",
+    "tmdbId": 91363,
+    "releaseDate": "2021-08-11"
+  },
+  {
+    "title": "Shang-Chi and the Legend of the Ten Rings",
+    "type": "movie",
+    "tmdbId": 566525,
+    "releaseDate": "2021-09-01"
+  },
+  {
+    "title": "Eternals",
+    "type": "movie",
+    "tmdbId": 524434,
+    "releaseDate": "2021-11-03"
+  },
+  {
+    "title": "Hawkeye",
+    "type": "series",
+    "tmdbId": 88329,
+    "releaseDate": "2021-11-24"
+  },
+  {
+    "title": "Spider-Man: No Way Home",
+    "type": "movie",
+    "tmdbId": 634649,
+    "releaseDate": "2021-12-15"
+  },
+  {
+    "title": "Moon Knight",
+    "type": "series",
+    "tmdbId": 92749,
+    "releaseDate": "2022-03-30"
+  },
+  {
+    "title": "Doctor Strange in the Multiverse of Madness",
+    "type": "movie",
+    "tmdbId": 453395,
+    "releaseDate": "2022-05-04"
+  },
+  {
+    "title": "Ms. Marvel",
+    "type": "series",
+    "tmdbId": 92782,
+    "releaseDate": "2022-06-08"
+  },
+  {
+    "title": "Thor: Love and Thunder",
+    "type": "movie",
+    "tmdbId": 616037,
+    "releaseDate": "2022-07-06"
+  },
+  {
+    "title": "She-Hulk: Attorney at Law",
+    "type": "series",
+    "tmdbId": 92783,
+    "releaseDate": "2022-08-18"
+  },
+  {
+    "title": "Werewolf by Night",
+    "type": "movie",
+    "tmdbId": 894205,
+    "releaseDate": "2022-09-25"
+  },
+  {
+    "title": "Black Panther: Wakanda Forever",
+    "type": "movie",
+    "tmdbId": 505642,
+    "releaseDate": "2022-11-09"
+  },
+  {
+    "title": "The Guardians of the Galaxy Holiday Special",
+    "type": "movie",
+    "tmdbId": 774752,
+    "releaseDate": "2022-11-24"
+  },
+  {
+    "title": "Ant-Man and the Wasp: Quantumania",
+    "type": "movie",
+    "tmdbId": 640146,
+    "releaseDate": "2023-02-15"
+  },
+  {
+    "title": "Guardians of the Galaxy Vol. 3",
+    "type": "movie",
+    "tmdbId": 447365,
+    "releaseDate": "2023-05-03"
+  },
+  {
+    "title": "Secret Invasion",
+    "type": "series",
+    "tmdbId": 114472,
+    "releaseDate": "2023-06-21"
+  },
+  {
+    "title": "The Marvels",
+    "type": "movie",
+    "tmdbId": 609681,
+    "releaseDate": "2023-11-08"
+  },
+  {
+    "title": "Echo",
+    "type": "series",
+    "tmdbId": 122226,
+    "releaseDate": "2024-01-09"
+  },
+  {
+    "title": "Deadpool & Wolverine",
+    "type": "movie",
+    "tmdbId": 533535,
+    "releaseDate": "2024-07-24"
+  },
+  {
+    "title": "Agatha All Along",
+    "type": "series",
+    "tmdbId": 138501,
+    "releaseDate": "2024-09-18"
+  },
+  {
+    "title": "Captain America: Brave New World",
+    "type": "movie",
+    "tmdbId": 822119,
+    "releaseDate": "2025-02-12"
+  },
+  {
+    "title": "Daredevil: Born Again",
+    "type": "series",
+    "tmdbId": 202555,
+    "releaseDate": "2025-03-04"
+  },
+  {
+    "title": "Thunderbolts*",
+    "type": "movie",
+    "tmdbId": 986056,
+    "releaseDate": "2025-04-30"
+  },
+  {
+    "title": "Ironheart",
+    "type": "series",
+    "tmdbId": 114471,
+    "releaseDate": "2025-06-24"
+  },
+  {
+    "title": "The Fantastic Four: First Steps",
+    "type": "movie",
+    "tmdbId": 617126,
+    "releaseDate": "2025-07-23"
+  }
 ];
