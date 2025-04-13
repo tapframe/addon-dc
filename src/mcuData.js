32abde178562a92255b2882093be399cafce809d
GitHub Action <action@github.com>
2025-04-13 22:47:03 +0000

    Monthly MCU data update

--- src/mcuData.js
+++ src/mcuData.js
@@ -1,2 +1,10 @@
 module.exports = [
-]
+  {
+    "title": "Thunderbolts*",
+    "type": "movie",
+    "imdbId": "tt20969586",
+    "id": "marvel_tt20969586",
+    "releaseYear": "2025",
+    "poster": "https://image.tmdb.org/t/p/original/hqcexYHbiTBfDIdDWxrxPtVndBX.jpg"
+  }
+];
